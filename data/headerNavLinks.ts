b6e5cb3da971ef73de8d5dbe4db941fa5cd2cb2c
yugami <yugamissu@gmail.com>
2025-07-11 19:18:16 +0800

    配置站点等个性化信息

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,8 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/projects', title: 'portfolio' },
+  { href: 'https://photos.incessantleo.com', title: 'photos' },
   { href: '/about', title: 'About' },
 ]
 
