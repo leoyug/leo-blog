518919aa20d33b823f3a025de03f22ae6ba5f749
yugami <yugamissu@gmail.com>
2025-07-15 01:08:02 +0800

    默认加载评论框

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,18 +5,12 @@ import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false)
+  const [loadComments] = useState(true) // 默认就是 true
 
   if (!siteMetadata.comments?.provider) {
     return null
   }
   return (
-    <>
-      {loadComments ? (
-        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
-      ) : (
-        <button onClick={() => setLoadComments(true)}>Load Comments</button>
-      )}
-    </>
+    <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
   )
 }
